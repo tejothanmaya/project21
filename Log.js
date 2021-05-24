778e644234cfc9e140562d63101974ef68a5e312
tejothanmaya <83295627+tejothanmaya@users.noreply.github.com>
2021-05-24 16:10:16 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,20 @@
+class Log{
+    constructor(speed){
+        this.x = random([100,300,500,700,900]);
+        this.y = height-random([550,650,750,1350,1450,1550]);
+        this.width = 1500;
+        this.height = 800;
+        this.speed = speed;
+        this.spt = createSprite(this.x,this.y,this.width,this.height);
+        this.spt.velocityX = this.speed;
+        this.spt.shapeColor = "brown";
+        this.spt.scale=0.1;
+        if(this.spt.y==height-250 || this.spt.y==height-1050 || this.spt.y==height-1850)
+        {
+           this.speed=-speed;
+        }
+        this.spt.velocityX = -speed;
+    }
+
+   
+}
